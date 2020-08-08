90a99de2592d42e37229c8e47d1a7d980bc2b5a2
Niphra01 <64540593+Niphra01@users.noreply.github.com>
2020-08-08 10:38:45 +0300

    Update unmute.js

--- commands/unmute.js
+++ commands/unmute.js
@@ -6,8 +6,7 @@ module.exports = {
     async execute(client, message, args, ops) {
         //if (message.author.id === "141311627373969408") return message.reply("Sadece 'SEN' bu yetkiyi kullanamazsın.");
         if (!args[0]) return message.reply('Lütfen kullanıcıyı belirtiniz. Örnek: n!unmute <@NiphraBOT>')
-        if (!message.member.permissions.has("KICK_MEMBERS")) return message.reply("Bu yetkiye sahip değilsiniz!");
-        if (message.channel.id === '526541910685384704') {
+        //if (!message.member.permissions.has("KICK_MEMBERS")) return message.reply("Bu yetkiye sahip değilsiniz!");
             const user = message.mentions.users.first();
             if (user) {
                 const member = message.guild.member(user);
@@ -15,18 +14,15 @@ module.exports = {
                     if (member.voice.channel) {
                         member.voice.setDeaf(false);
                         member.voice.setMute(false);
-                        message.channel.bulkDelete(1);
+                        //message.channel.bulkDelete(1);
                     } else { message.reply("Üye sesli bir kanalda değil.") }
                 } else {
-                    message.reply("Serverda böyle bir üye yok.");
+                    message.reply("Sunucuda böyle bir üye yok.");
                 }
             }
             else {
-                message.reply("Serverda böyle bir üye yok.");
+                message.reply("Sunucuda böyle bir üye yok.");
             }
-        }
-        else {
-            message.channel.send('Bu komut sadece **musicbot** adlı kanalda çalışır')
-        }
+        
     }
 }
