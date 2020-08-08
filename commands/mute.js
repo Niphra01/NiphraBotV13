ef57f0a4bb236a3f56b9d66be15318241868a200
Niphra01 <64540593+Niphra01@users.noreply.github.com>
2020-08-08 10:38:59 +0300

    Update mute.js

--- commands/mute.js
+++ commands/mute.js
@@ -8,7 +8,7 @@ module.exports = {
         //if (message.author.id === "141311627373969408") return message.reply("Sadece 'SEN' bu yetkiyi kullanamazsın.");
         if (!args[0]) return message.reply('Lütfen kullanıcıyı belirtiniz. Örnek: n!mute <@NiphraBOT>')
         if (!message.member.permissions.has("KICK_MEMBERS")) return message.reply("Bu yetkiye sahip değilsiniz!");
-        if (message.channel.id === '526541910685384704') {
+       
             const user = message.mentions.users.first();
             if (user) {
                 const member = message.guild.member(user);
@@ -16,20 +16,17 @@ module.exports = {
                     if (member.voice.channel) {
                         member.voice.setDeaf(true);
                         member.voice.setMute(true);
-                        message.channel.bulkDelete(1);
+                        //message.channel.bulkDelete(1);
                     } else { message.reply("Üye sesli bir kanalda değil.") }
 
                 } else {
-                    message.reply("Serverda böyle bir üye yok.");
+                    message.reply("Sunucuda böyle bir üye yok.");
                 }
             }
             else {
-                message.reply("Serverda böyle bir üye yok.");
+                message.reply("Sunucuda böyle bir üye yok.");
             }
-        }
-        else {
-            message.channel.send('Bu komut sadece musicbot adlı kanalda çalışır')
-        }
+        
     }
 
 }
