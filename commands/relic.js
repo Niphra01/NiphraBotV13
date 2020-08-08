b6a119d0617edd030803ef949a68ae1ab9b1a2e6
Niphra01 <64540593+Niphra01@users.noreply.github.com>
2020-08-08 10:38:57 +0300

    Update relic.js

--- commands/relic.js
+++ commands/relic.js
@@ -8,19 +8,18 @@ module.exports = {
     aliases: 'relic',
     description: 'Relic`lerin bilgilerini gösterir',
     async execute(client, message, args, ops) {
-      try{
+            
+            if(!relicConfig[args[0].toUpperCase()].name[args[1].toUpperCase()]) return  message.channel.send("Girdiğiniz relic Vault'ta veya böyle bir relic yok");
             console.log('' + relicConfig[args[0].toUpperCase()].drops[args[1].toUpperCase()])
             const relicEmbed = new Discord.MessageEmbed()
                 .setTitle(`${args[0].toUpperCase()} ` + ` ${relicConfig[args[0].toUpperCase()].name[args[1].toUpperCase()]}`)
                 .addFields(
                     { name: `Best Drop Location`, value: `${relicConfig[args[0].toUpperCase()].location[args[1].toUpperCase()]}` },
-                    { name: `Inside Drop`, value: `${relicConfig[args[0].toUpperCase()].drops[args[1].toUpperCase()]}`, inline: true }
+                    { name: `Inside Drop`, value: `${relicConfig[args[0].toUpperCase()].drops[args[1].toUpperCase()]}`, inline: true }					
                 )
+				message.channel.send(relicEmbed);
 
-
-            message.channel.send(relicEmbed);
-        }catch(err){
-            message.channel.send("Girdiğiniz relic Vault'ta veya böyle bir relic yok")
-        }
+            
+   
     }
 }
