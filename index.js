6f9eb5e659304027e5c26594d770bf870f0556a4
Niphra01 <64540593+Niphra01@users.noreply.github.com>
2020-09-06 01:01:43 +0300

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,11 @@ const client = new discord.Client({ disableEveryone: true, disabledEvents: ["TYP
 const { readdirSync } = require("fs");
 const { join } = require("path");
 const PREFIX = process.env.PREFIX;
+const moment = require('moment');
+const sql = require('sqlite')
+
+
+sql.open('./Logs.sqlite');
 
 //CLIENT EVENTS
 client.on("ready", () => {
@@ -58,12 +63,12 @@ client.on("guildMemberRemove", member => {
   const memberCountChannel = armutSporGuild.channels.cache.get('714001061681168384')
   memberCountChannel.setName(`Üye Sayısı :  ` + memberCount)
 });
-client.on('messageDelete',async message => {
-try{
-  if (message.channel.id === '526541910685384704' || message.author.bot || message.attachments.first()||message.guild.id !=='249951409070407681') return
-}catch(err){
-  console.log('Mesaj silme başarı ile gerçekleşti fakat Hata alındı')
-}
+client.on('messageDelete', async message => {
+  try {
+    if (message.channel.id === '526541910685384704' || message.author.bot || message.attachments.first() || message.guild.id !== '249951409070407681') return
+  } catch (err) {
+    console.log('Mesaj silme başarı ile gerçekleşti fakat Hata alındı')
+  }
 
   if (!message.partials) {
     const channel = client.channels.cache.get('712110998286499930')
@@ -87,10 +92,28 @@ try{
 )
 
 //WHEN SOMEONE MESSAGE
-client.on("message",async message => {
+client.on("message", async message => {
   if (message.author.bot) return;
   if (!message.guild) return;
-
+  let tStamp = moment().format('LLLL'); //creates time stamp
+  if(message.attachments.first()) return;
+  else{
+  if (message.channel.type == 'dm') { //checks for DM - Creates a DM DB and records
+    let dmName = `${message.author.username}DM`;
+    sql.run(`INSERT INTO ${dmName} (username, message, timestamp, userID) VALUES (?,?,?,?)`, [message.author.username, message.content, tStamp, message.author.id]).catch(() => {
+      sql.run(`CREATE TABLE IF NOT EXISTS ${dmName} (username TEXT, message TEXT, timestamp TEXT, userID TEXT);`).then(() => {
+        sql.run(`INSERT INTO ${dmName} (username, message, timestamp, userID) VALUES (?,?,?,?)`, [message.author.username, message.content, tStamp, message.author.id]);
+      })
+    })
+  } else { //otherwise it's a normal channel
+    sql.run(`INSERT INTO ${message.channel.name} (username, message, timestamp, userID) VALUES (?,?,?,?)`, [message.author.username, message.content, tStamp, message.author.id]).catch(() => {
+      console.error;
+      sql.run(`CREATE TABLE IF NOT EXISTS ${message.channel.name} (username TEXT, message TEXT, timestamp TEXT, userID TEXT);`).then(() => {
+        sql.run(`INSERT INTO ${message.channel.name} (username, message, timestamp, userID) VALUES (?,?,?,?)`, [message.author.username, message.content, tStamp, message.author.id]);
+      }) // KNOWN ISSUE: Does not like channels with "-" in them
+    })
+  }
+}
   if (message.content.startsWith(PREFIX)) { //IF MESSSAGE STARTS WITH MINE BOT PREFIX
 
     const args = message.content.slice(PREFIX.length).trim().split(/ +/) //removing prefix from args
@@ -110,17 +133,17 @@ client.on("message",async message => {
   }
   let chatFilter = ['YASAK'];
   const wordPicker = message.content.toUpperCase().split(' ')
-  for(var i=0;i < chatFilter.length;i++){
-    if(wordPicker.includes(chatFilter[i])){
-      try{
+  for (var i = 0; i < chatFilter.length; i++) {
+    if (wordPicker.includes(chatFilter[i])) {
+      try {
         await message.delete();
         await message.reply('Yasaklı kelime kullandın');
-      }catch(err){
+      } catch (err) {
         console.log('Hata')
       }
     }
   }
-  
+
 });
 
 
