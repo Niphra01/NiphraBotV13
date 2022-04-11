f3cbccd265a730db148dc9f13099b519204a852c
Niphra01 <Niphra01@gmail.com>
2022-04-11 12:39:33 +0300

    Create calculator.js

---
+++ commands/calculator.js
@@ -0,0 +1,25 @@
+const math = require("math-expression-evaluator");
+const discord = require("discord.js");
+
+module.exports = {
+    name: "calculator",
+    aliases: 'calculator',
+    description: "Hesap makinesi",
+    async execute(client, message, args) {
+
+        if (!args[0]) return message.channel.send("Bir değer giriniz!");
+        let resp;
+        try {
+            resp = math.eval(args.join(''));
+        } catch (e) {
+            return message.channel.send("Geçerli bir değer giriniz!");
+        }
+        const embed = new discord.MessageEmbed()
+            .setColor(0xffffff)
+            .setTitle("Matematik hesaplaması")
+            .addField("Girdi", `\`\`\`js\n${args.join('')}\`\`\``)
+            .addField("Çıktı", `\`\`\`js\n${resp}\`\`\``)
+
+        message.channel.send({ embeds: [embed] });
+    }
+}
