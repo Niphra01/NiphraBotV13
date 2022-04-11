0e24295bc9a76d1c1dc39e98520bc39ca7ada5d2
Niphra01 <Niphra01@gmail.com>
2022-04-11 12:39:37 +0300

    Create delete.js

---
+++ commands/delete.js
@@ -0,0 +1,21 @@
+const discord = require("discord.js");
+module.exports = {
+  name: "delete",
+  aliases: ':recycle: delete',
+  description: "Girilen değer kadar mesajları siler.(14 günden önceki mesajlar silinmez)",
+  async execute(client, message, args) {
+    if (!message.member.permissions.has("MANAGE_MESSAGES")) return message.reply("Bu yetkiye sahip değilsiniz!");
+    if (!args[0]) return message.channel.send("Değer giriniz.");
+    try {
+      message.channel.bulkDelete(parseInt(args[0]) + 1).then(() => {
+        message.channel.send(`${args[0]} mesaj silindi.`).then(message => message.delete({ timeout: 5000 }));
+      }).catch(err => {
+        console.log('14 günden önceki mesaj silinmeye çalışıldı')
+        message.reply("14 günden önceki mesajları silemezsin");
+      });
+    } catch (err) {
+      message.reply("14 günden önceki mesajları silemezsin");
+      console.log('14 günden önceki mesaj silinmeye çalışıldı')
+    }
+  }
+}
