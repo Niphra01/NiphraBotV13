57a7edc5351b00150a6888c12b3e2c2a976cbca1
Niphra01 <Niphra01@gmail.com>
2022-05-18 13:17:13 +0300

    update

--- commands/delete.js
+++ commands/delete.js
@@ -9,11 +9,11 @@ module.exports = {
       return message.channel.send(
         "Set how many messages do you want to delete. Example <-delete 5>"
       );
-    const deleteCount = parseInt(args[0], true);
+    const deleteCount = parseInt(args[0], 10);
     if (!deleteCount || deleteCount < 1 || deleteCount > 100) return;
 
     message.channel.bulkDelete(deleteCount + 1)
-      .then(message => { message.channel.send(`${args[0]} message deleted`)})
+      .then(message => { message.channel.send(`${args[0]} message deleted`) })
       .catch(error => console.log(`Couldn't delete messages because of: ${error}`));
   },
 };
