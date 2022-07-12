2164217729b151ac2e76ff92bd6eaf3e0dd57d2c
Niphra01 <Niphra01@gmail.com>
2022-07-12 16:12:58 +0300

    added TR words

--- commands/games/wordle.js
+++ commands/games/wordle.js
@@ -6,21 +6,32 @@ module.exports = {
     name: "wordle",
     description: "Wordle Game - Try to find words with 5 letter",
     async execute(client, message, args) {
-        const alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "y", "z"]
-        let randomWord, randomLetter, blackListedWords = [], guesses = []
+        const alphabetEN = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "y", "z"]
+        const alphabetTR = ["a", "b", "c", "ç", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "ö", "p", "r", "s", "ş", "t", "u", "ü", "v", "y", "z"]
+        let randomWord, randomLetter, blackListedWords = [], guesses = [], tempWord = []
 
-        if (args[0].length != 5) {
-            return message.reply({ content: "You need to type word with 5 letter" })
+        if (args[0] === undefined || args[0].length != 2) {
+            return message.reply({ content: "Type your language first `tr`,`en`" })
         } else {
+
+            if (args[0] === "tr") {
+                randomLetter = alphabetTR[Math.floor(Math.random() * alphabetTR.length)]
+                randomWord = words.tr[randomLetter][Math.floor(Math.random() * words.tr[randomLetter].length)]
+            }
+            else if (args[0] === "en") {
+                randomLetter = alphabetEN[Math.floor(Math.random() * alphabetEN.length)]
+                randomWord = words.en[randomLetter][Math.floor(Math.random() * words.en[randomLetter].length)]
+            }
 
             if (user.some(u => u === message.author.id)) {
                 message.reply({ content: `You already started the game give your answer without -wordle` })
             } else {
 
-                user.push(message.author.id)
                 console.log(user)
-                randomLetter = alphabet[Math.floor(Math.random() * alphabet.length)]
-                randomWord = words.en[randomLetter][Math.floor(Math.random() * words.en[randomLetter].length)]
+                console.log(randomWord)
+
+                user.push(message.author.id)
+
                 const canvas = Canvas.createCanvas(330, 397);
                 const context = canvas.getContext('2d');
 
@@ -31,47 +42,16 @@ module.exports = {
                 context.textAlign = 'center'
                 context.fillStyle = '#d7dadc';
 
-
-                const absentSquare = await Canvas.loadImage('./src/images/ColorAbsent.png');
                 const emptySquare = await Canvas.loadImage('./src/images/EmptySquare.png');
-                const greenSquare = await Canvas.loadImage('./src/images/GreenSquare.png');
-                const yellowSquare = await Canvas.loadImage('./src/images/YellowSquare.png');
-                let square = absentSquare;
 
                 let squareSize = 62;
                 let rowOffset = 0;
                 let buffer = 0;
-                if (guesses == "") {
-                    guesses[0] = args[0];
-                }
-                else {
-                    guesses.push(args[0]);
-                }
+
 
                 for (var j = 0; j < 6; j++) {
                     for (var i = 0; i < 5; i++) {
-
-                        let imageNumber
-                        if (guesses[j] === undefined) { imageNumber = 0; }
-                        //letter is in word at same spot
-                        else if (guesses[j].charAt(i) == randomWord.charAt(i)) { imageNumber = 1; }
-                        //letter is in word at different spot
-                        else if (randomWord.includes(guesses[j].charAt(i))) { imageNumber = 2; }
-                        //letter is not in word
-                        else {
-                            imageNumber = 3;
-                            if (blackListedWords.some(el => el.includes(args[0][i]))) {
-                            } else {
-                                blackListedWords.push(args[0][i])
-                            }
-                        }
-
-                        if (imageNumber == 0) { square = emptySquare; }
-                        else if (imageNumber == 1) { square = greenSquare; }
-                        else if (imageNumber == 2) { square = yellowSquare; }
-                        else if (imageNumber == 3) { square = absentSquare; }
-
-                        context.drawImage(square, i * squareSize + buffer, rowOffset, squareSize, squareSize);
+                        context.drawImage(emptySquare, i * squareSize + buffer, rowOffset, squareSize, squareSize);
                         if (guesses[j] != undefined) {
                             context.fillText(guesses[j].charAt(i), (squareSize / 2) + buffer + squareSize * i, rowOffset + 45);
                         }
@@ -92,10 +72,13 @@ module.exports = {
 
                     let value = collected.content
                     if (value.length != 5) { return message.reply({ content: "You need to type word with 5 letter" }) }
+                    if (guesses == "") {
+                        guesses[0] = value;
+                    }
+                    else {
+                        guesses.push(value);
+                    }
 
-                    guesses.push(value);
-
-                    console.log(user)
                     const canvas = Canvas.createCanvas(330, 397);
                     const context = canvas.getContext('2d');
 
@@ -123,9 +106,13 @@ module.exports = {
                             let imageNumber
                             if (guesses[j] === undefined) { imageNumber = 0; }
                             //letter is in word at same spot
-                            else if (guesses[j].charAt(i) == randomWord.charAt(i)) { imageNumber = 1; }
+                            else if (guesses[j].charAt(i) == randomWord.charAt(i)) {
+                                imageNumber = 1;
+                            }
                             //letter is in word at different spot
-                            else if (randomWord.includes(guesses[j].charAt(i))) { imageNumber = 2; }
+                            else if (randomWord.includes(guesses[j].charAt(i))) {
+                                imageNumber = 2;
+                            }
                             //letter is not in word
                             else {
                                 imageNumber = 3;
@@ -154,7 +141,6 @@ module.exports = {
 
                         buffer = 0;
                         rowOffset += squareSize + 5;
-
                     }
                     const BlEmbed = new MessageEmbed().setTitle(`Blacklisted Words: ${blackListedWords.toString().toUpperCase()}`)
                     const attachment2 = new MessageAttachment(canvas.toBuffer(), 'wordle.png');
